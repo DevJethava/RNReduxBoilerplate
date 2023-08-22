75ef8498cdb917ecb41fa34ea46a75e8fb4390f5
Dev Jethava <dev.jethava@cybercom.in>
2023-08-22 23:43:35 +0530

    * add packages

--- App.tsx
+++ App.tsx
@@ -5,114 +5,39 @@
  * @format
  */
 
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import type { PropsWithChildren } from 'react';
 import {
     SafeAreaView,
-    ScrollView,
     StatusBar,
     StyleSheet,
     Text,
-    useColorScheme,
     View,
-} from 'react-native';
+} from "react-native";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-import {
-    Colors,
-    DebugInstructions,
-    Header,
-    LearnMoreLinks,
-    ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-    title: string;
-}>;
-
-function Section({ children, title }: SectionProps): JSX.Element {
-    const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
     return (
-        <View style={styles.sectionContainer}>
-            <Text
-                style={[
-                    styles.sectionTitle,
-                    {
-                        color: isDarkMode ? Colors.white : Colors.black,
-                    },
-                ]}>
-                {title}
-            </Text>
-            <Text
-                style={[
-                    styles.sectionDescription,
-                    {
-                        color: isDarkMode ? Colors.light : Colors.dark,
-                    },
-                ]}>
-                {children}
-            </Text>
-        </View>
-    );
-}
-
-function App(): JSX.Element {
-    const isDarkMode = useColorScheme() === 'dark';
-
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    };
-
-    return (
-        <SafeAreaView style={backgroundStyle}>
-            <StatusBar
-                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-                backgroundColor={backgroundStyle.backgroundColor}
-            />
-            <ScrollView
-                contentInsetAdjustmentBehavior="automatic"
-                style={backgroundStyle}>
-                <Header />
-                <View
-                    style={{
-                        backgroundColor: isDarkMode ? Colors.black : Colors.white,
-                    }}>
-                    <Section title="Step One">
-                        Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-                        screen and then come back to see your edits.
-                    </Section>
-                    <Section title="See Your Changes">
-                        <ReloadInstructions />
-                    </Section>
-                    <Section title="Debug">
-                        <DebugInstructions />
-                    </Section>
-                    <Section title="Learn More">
-                        Read the docs to discover what to do next:
-                    </Section>
-                    <LearnMoreLinks />
+        <NavigationContainer>
+            <SafeAreaView className="flex">
+                <StatusBar
+                    barStyle={'dark-content'}
+                />
+                <View className="bg-white p-2 h-full justify-center items-center">
+                    <View className="flex-row">
+                        <Text className="text-2xl text-center">Hello </Text>
+                        <Text className="text-center" style={styles.fontSize}>World!</Text>
+                    </View>
                 </View>
-            </ScrollView>
-        </SafeAreaView>
+            </SafeAreaView>
+        </NavigationContainer>
     );
 }
 
 const styles = StyleSheet.create({
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-    },
-    highlight: {
-        fontWeight: '700',
-    },
+    fontSize: {
+        fontSize: hp(3)
+    }
 });
 
 export default App;
--- README.md
+++ README.md
@@ -10,5 +10,5 @@ To learn more about Project, take a look at the following resources:
 - [React Native Navigation](https://reactnavigation.org/docs/getting-started) - React Navigation is made up of some core utilities and those are then used by navigators to create the navigation structure in your app.
 - [Redux](https://redux.js.org/introduction/getting-started) - **Redux** is a predictable state container for JavaScript apps.
 - [Redux Toolkit](https://redux.js.org/introduction/installation) - Redux Toolkit includes the Redux core, as well as other key packages we feel are essential for building Redux applications (such as Redux Thunk and Reselect).
-- [NativeWind](https://www.nativewind.dev/quick-starts/react-native-cli) - a **guided tour** of the React Native **basics**.
-- [Blog](https://reactnative.dev/blog) - NativeWind uses Tailwind CSS as scripting language to create a **universal styling system**.
+- [NativeWind](https://www.nativewind.dev/quick-starts/react-native-cli) - NativeWind uses Tailwind CSS as scripting language to create a **universal styling system**.
+- [Axios]() - API calling library.
